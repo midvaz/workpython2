6ae9601c41ec1f9b182736d6d52b06fd79abcc46
midvaz <mi28df@gmail.com>
2020-04-25 17:15:06 +0300

    v 3.6/54b

--- testprog.py
+++ testprog.py
@@ -1,17 +1,3 @@
-students = dict()
-rating = dict()
-
-students['1'] = 'Олег'
-students['2'] = 'Александр'
-students['3'] = 'Максим'
-students['4'] = 'Игорь'
-students['5'] = 'Чувак'
-
-rating['1'] = 0
-rating['2'] = 0
-rating['3'] = 0
-rating['4'] = 0
-rating['5'] = 0
-
-i = 1
-print(len(rating))
+a = 55
+if a > 5 :
+    print (" куку нахуй ")
